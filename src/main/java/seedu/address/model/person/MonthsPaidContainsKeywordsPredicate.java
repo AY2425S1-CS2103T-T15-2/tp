aa086ecd86dbdcc8bfad1d82c84819242865ca94
Wai Hin <138366109+waihin26@users.noreply.github.com>
2024-10-23 21:35:47 +0800

    Remove unused import

--- src/main/java/seedu/address/model/person/MonthsPaidContainsKeywordsPredicate.java
+++ src/main/java/seedu/address/model/person/MonthsPaidContainsKeywordsPredicate.java
@@ -2,7 +2,6 @@ package seedu.address.model.person;
 
 import java.util.List;
 import java.util.function.Predicate;
-import java.util.regex.Pattern;
 
 import seedu.address.commons.util.StringUtil;
 import seedu.address.commons.util.ToStringBuilder;
